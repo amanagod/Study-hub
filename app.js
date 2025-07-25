52dde6334a5e2d613b47698cb5f010e3bed2f5d2
amanagod <nishantpanchal2411@gmail.com>
2025-07-25 21:14:51 +0530

    port solved

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const Data = require('./models/data.js');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.port; 
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
